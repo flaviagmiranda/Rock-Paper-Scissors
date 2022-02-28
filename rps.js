b0ca92158cbf81a059de18eff5cdf401f502cbef
Flavia Miranda <flaviagracianom@gmail.com>
2022-02-28 17:18:56 +0000

    removing test

--- rps.js
+++ rps.js
@@ -6,8 +6,6 @@ function computerPlay() {
   return randomElement
 }
 
-var hey = 'hello'
-
 // //Step 2: Writing a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock". Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
 // function singleRound(playerSelection, computerSelection) {
